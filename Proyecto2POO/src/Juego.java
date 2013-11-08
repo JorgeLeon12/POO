b19be7e833ec85fff88fcd9489047acab8e15fcc
JorgeLeon12 <jorgeleon12@hotmail.com>
2013-11-07 20:02:07 -0600

    Juego mas Funcional (solo imagenes)

--- Proyecto2POO/src/Juego.java
+++ Proyecto2POO/src/Juego.java
@@ -10,11 +10,16 @@ import java.lang.Math;
 import java.awt.Color;
 import javax.swing.JPanel;
 
+import javax.swing.*;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 public class Juego extends JPanel{
 
-	private BufferedImage image, image2;
-	private int num1, num2;
+	private BufferedImage imgNum, imgNum2, imgSig, imgInt, imgIgu;
+	private int num1, num2, coor1, coor2, coor3;
 	private String dificultad = "";
+	public JTextField respUsr;
 
 	public Juego(){
 		super();		
@@ -32,17 +37,59 @@ public class Juego extends JPanel{
 
 	public Juego(String difi){
 		dificultad = difi;
-		num1 = (int) Math.round((Math.random()*9));
-		num2 = (int) Math.round((Math.random()*9));
-		String strNum1 = ".//Numeros//" + (Integer.toString(num1)) + ".png";
-		String strNum2 = ".//Numeros//" + (Integer.toString(num2)) + ".png";
+		num1 = (int) Math.round((Math.random()*8+1));
+		num2 = (int) Math.round((Math.random()*8+1));
+		String strNum1 = "./Numeros/" + (Integer.toString(num1)) + ".png";
+		String strNum2 = "./Numeros/" + (Integer.toString(num2)) + ".png";
+
 		if(dificultad == "Principiante"){
+		}else if(dificultad == "Sencillo"){//----------------------------------Sensillo----------------------------------------------
 			System.out.println(dificultad);
 			try {
-				image = ImageIO.read(new File(strNum1));
-				image2 = ImageIO.read(new File(strNum2));
+				coor1 = 250;
+				coor2 = 455;
+				coor3 = 345;
+				imgNum = ImageIO.read(new File(strNum1));
+				imgNum2 = ImageIO.read(new File(strNum2));
+				imgSig = ImageIO.read(new File("./Numeros/+.png"));
 			}catch (IOException ex) {
-				System.out.println("Error con la imagen");
+				System.out.println("Error con la imgNumn");
+			}
+		}else if(dificultad == "Retador"){//----------------------------------Retador----------------------------------------------
+			System.out.println(dificultad);
+			try {
+				coor1 = 250;
+				coor2 = 455;
+				coor3 = 345;
+				imgNum = ImageIO.read(new File(strNum1));
+				imgNum2 = ImageIO.read(new File(strNum2));
+				imgSig = ImageIO.read(new File("./Numeros/+.png"));
+			}catch (IOException ex) {
+				System.out.println("Error con la imgNumn");
+			}
+		}else if(dificultad == "Avanzado"){//----------------------------------Avanzado----------------------------------------------
+			System.out.println(dificultad);
+			try {
+				coor1 = 250;
+				coor2 = 455;
+				coor3 = 345;
+				imgNum = ImageIO.read(new File(strNum1));
+				imgNum2 = ImageIO.read(new File(strNum2));
+				imgSig = ImageIO.read(new File("./Numeros/+.png"));
+			}catch (IOException ex) {
+				System.out.println("Error con la imgNumn");
+			}
+		}else if(dificultad == "DIOS"){//----------------------------------OH GOD WHY!?----------------------------------------------
+			System.out.println(dificultad);
+			try {
+				coor1 = 250;
+				coor2 = 455;
+				coor3 = 345;
+				imgNum = ImageIO.read(new File(strNum1));
+				imgNum2 = ImageIO.read(new File(strNum2));
+				imgSig = ImageIO.read(new File("./Numeros/+.png"));
+			}catch (IOException ex) {
+				System.out.println("Error con la imgNumn");
 			}
 		}
     }
@@ -50,7 +97,8 @@ public class Juego extends JPanel{
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(image, 250, 250, null);
-        g.drawImage(image2, 300,250,null);
+        g.drawImage(imgNum, this.coor1, 250, null);
+       	g.drawImage(imgNum2, coor2,250,null);
+       	g.drawImage(imgSig, coor3,250,null);
     }
 }
