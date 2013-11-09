21b526a84a437fcd9896695a1d2c2ecb1d0b96d2
Cesar Robles <chechare_94@hotmail.com>
2013-11-08 22:55:27 -0600

    Niveles rideseñado

--- Proyecto2POO/src/Niveles.java
+++ Proyecto2POO/src/Niveles.java
@@ -1,14 +1,14 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.imageio.ImageIO;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Niveles extends JPanel implements ActionListener {
@@ -18,32 +18,46 @@ public class Niveles extends JPanel implements ActionListener {
 	private BufferedImage image;
 
 	public Niveles(){
-		this.setLayout(null);
-
+		super();
+		this.setLayout(new BorderLayout());
+		JPanel superior=new JPanel();
+		JPanel contenedor=new JPanel();
+		JPanel inferior=new JPanel();	
+		contenedor.setLayout(new FlowLayout(FlowLayout.CENTER,1000,60));
+		inferior.setLayout(new FlowLayout(FlowLayout.RIGHT,10,10));
+		Font myFont= new Font ("Comic Sans MS",1,22);
+	
+		superior.setPreferredSize(new Dimension(800,50));
+		inferior.setPreferredSize(new Dimension(800,50));		
+		JLabel titulo=new JLabel("Selecciona una partida");
+		titulo.setFont(myFont);
 		principiante=new JButton("Principiante");
 		facil=new JButton("Sencillo");
 		medio=new JButton("Retador");
 		dificil=new JButton("Avanzado");
 		cancelar=new JButton("Cancelar");
+		
+		principiante.setPreferredSize(new Dimension(200,30));
+		facil.setPreferredSize(new Dimension(200,30));
+		medio.setPreferredSize(new Dimension(200,30));
+		dificil.setPreferredSize(new Dimension(200,30));
+		cancelar.setPreferredSize(new Dimension(200,30));
 
 		principiante.addActionListener(this);
 		facil.addActionListener(this);
 		medio.addActionListener(this);
 		dificil.addActionListener(this);
 		cancelar.addActionListener(this);
-
-		principiante.setBounds(300,130,200,30);
-		facil.setBounds(300,220,200,30);
-		medio.setBounds(300,310,200,30);
-		dificil.setBounds(300,400,200,30);
-		cancelar.setBounds(690,530,100,30);
-
-
-		this.add(principiante);
-		this.add(facil);
-		this.add(medio);
-		this.add(dificil);
-		this.add(cancelar);
+		
+		contenedor.add(principiante);
+		contenedor.add(facil);
+		contenedor.add(medio);
+		contenedor.add(dificil);
+		inferior.add(cancelar);
+		superior.add(titulo);
+		this.add(superior, BorderLayout.NORTH);
+		this.add(contenedor, BorderLayout.CENTER);
+		this.add(inferior, BorderLayout.SOUTH);
 	}
 
 	public void seleccionarDificultad(String dificultad){
